31cd561b536cebf43004bb45fbefffaa280d78c4
Chris Foley <code@cfoley.co.uk>
2018-02-09 19:46:51 +0000

    Create matrix for up to one character.

--- diff.js
+++ diff.js
@@ -1,5 +1,46 @@
 document.getElementById("test-output").innerHTML = runTests();
 
 function runTests() {
-	return "test results";
+	let results = [
+		assertMatrix([[0]], "", ""),
+		assertMatrix([[0,1]], "", "a"),
+		assertMatrix([[0],[1]], "a", ""),
+		assertMatrix([[0,1],[1,0]], "a", "a"),
+		assertMatrix([[0,1],[1,0]], "a", "b"),
+	];
+	let summary = results.map(x => x[0] ? '.' : 'F').join("");
+	let failuredetails = results.filter(x => !x[0]).map(x => '\n' + x[1]).join("");
+	return summary + failuredetails;
+}
+
+function assertMatrix(expected, a, b) {
+	let actual = editMatrix(a, b);
+	let message = `Matrix for '${a}' and '${b}' was ${actual.join("|")}.`;
+	return [isMatrixEqual(expected, actual), message];
+}
+
+function isMatrixEqual(a, b) {
+	if (a.length != b.length) return false;
+	for (var r = 0; r < a.length; r++) {
+		if (a[r].length != b[r].length) return false;
+		for (var c = 0; c < a[r].length; c++) {
+			if (a[r][c] != b[r][c]) return false;
+		}
+	}
+	return true;
+}
+
+function editMatrix(a, b) {
+	let rows = a.length + 1;
+	let cols = b.length + 1;
+	let result = new Array(rows)
+	for (var r = 0; r < rows; r++) result[r] = new Array(cols);
+	for (var c = 0; c < cols; c++) result[0][c] = c;
+	for (var r = 1; r < rows; r++) result[r][0] = 1;
+	for (var r = 1; r < rows; r++) {
+		for (var c = 1; c < cols; c++) {
+			result[r][c] = (a[r]==b[c] ? 0 : 1);
+		}
+	}
+	return result;
 }
