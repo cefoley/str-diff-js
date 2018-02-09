848862c3387932e7f5839fbae3965614d12aa323
Chris Foley <code@cfoley.co.uk>
2018-02-09 20:04:16 +0000

    Builds string matrices for string lengths of 2.

--- diff.js
+++ diff.js
@@ -6,7 +6,11 @@ function runTests() {
 		assertMatrix([[0,1]], "", "a"),
 		assertMatrix([[0],[1]], "a", ""),
 		assertMatrix([[0,1],[1,0]], "a", "a"),
-		assertMatrix([[0,1],[1,0]], "a", "b"),
+		assertMatrix([[0,1],[1,1]], "a", "b"),
+		assertMatrix([[0,1,2],[1,0,1],[2,1,0]], "ab", "ab"),
+		assertMatrix([[0,1,2],[1,0,1],[2,1,1]], "ab", "ac"),
+		assertMatrix([[0,1,2],[1,1,2],[2,2,1]], "ab", "xb"),
+		assertMatrix([[0,1,2],[1,1,2],[2,2,2]], "ab", "xy"),
 	];
 	let summary = results.map(x => x[0] ? '.' : 'F').join("");
 	let failuredetails = results.filter(x => !x[0]).map(x => '\n' + x[1]).join("");
@@ -15,7 +19,7 @@ function runTests() {
 
 function assertMatrix(expected, a, b) {
 	let actual = editMatrix(a, b);
-	let message = `Matrix for '${a}' and '${b}' was ${actual.join("|")}.`;
+	let message = `Matrix for '${a}' and '${b}'. Expected [${expected.join("|")}] but was [${actual.join("|")}].`;
 	return [isMatrixEqual(expected, actual), message];
 }
 
@@ -33,14 +37,14 @@ function isMatrixEqual(a, b) {
 function editMatrix(a, b) {
 	let rows = a.length + 1;
 	let cols = b.length + 1;
-	let result = new Array(rows)
-	for (var r = 0; r < rows; r++) result[r] = new Array(cols);
-	for (var c = 0; c < cols; c++) result[0][c] = c;
-	for (var r = 1; r < rows; r++) result[r][0] = 1;
+	let m = new Array(rows)
+	for (var r = 0; r < rows; r++) m[r] = new Array(cols);
+	for (var c = 0; c < cols; c++) m[0][c] = c;
+	for (var r = 1; r < rows; r++) m[r][0] = r;
 	for (var r = 1; r < rows; r++) {
 		for (var c = 1; c < cols; c++) {
-			result[r][c] = (a[r]==b[c] ? 0 : 1);
+			m[r][c] = Math.min(m[r-1][c-1], m[r][c-1], m[r-1][c]) + (a[r-1]==b[c-1] ? 0 : 1);
 		}
 	}
-	return result;
+	return m;
 }
